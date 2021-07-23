f1ba33898066ce67fb53406a086829694679f86e
Ferran Rosales <f.rosales.lopera@accenture.com>
2021-07-23 10:56:33 +0200

    Added pod files.

---
+++ Marvel Heroes/Managers/Constants.swift
@@ -0,0 +1,19 @@
+//
+//  Constants.swift
+//  Marvel Heroes
+//
+//  Created by f.rosales.lopera on 20/7/21.
+//
+
+import Foundation
+
+struct Constants {
+    
+    static let basePath = "https://gateway.marvel.com/v1/public/characters?"
+    static let privateKey = "8a219b98a93d5b670b19067ddf5e9b6f16bbaebd"
+    static let publicKey = "23e6ade1932e49e3748db262d9738e02"
+    static let limit = 100
+    static let application = "application/json"
+    static let data = "data"
+    static let results = "results"
+}
