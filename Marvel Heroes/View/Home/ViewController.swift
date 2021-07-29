061653d208c0b2a0c421cb46a4c09b54d5d381d6
Ferran Rosales <f.rosales.lopera@accenture.com>
2021-07-29 07:53:37 +0200

    Added Launch Screen

--- Marvel Heroes/View/Home/ViewController.swift
+++ Marvel Heroes/View/Home/ViewController.swift
@@ -236,46 +236,7 @@ class ViewController: UIViewController {
 
 
 
-extension ViewController: UISearchBarDelegate {
-    
-    
-    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        
-        searchBar.resignFirstResponder()
-        self.allHeroes = self.heroes
-        self.heroes = []
-        let nameSearch = searchBar.searchTextField.text
-        MarvelAPi.loadHeroes(name: nameSearch, page: self.currentPage) { (info) in
-            if let info = info {
-                self.heroes += info.data.results
-                self.total = info.data.total
-                DispatchQueue.main.async {
-                    
-                    self.loadingHeroes = false
-                    self.tableView.reloadData()
-                    
-                    
-                }
-            }
-        }
-    }
-    
-    
-    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-        
-        if searchText.count == 0 {
-            DispatchQueue.main.async {
-                self.searchHeroes.resignFirstResponder()
-            }
-            self.heroes = self.allHeroes
-            self.loadingHeroes = false
-            self.tableView.reloadData()
-            
-        }
-    }
-    
-  
-}
+
 
 
 
